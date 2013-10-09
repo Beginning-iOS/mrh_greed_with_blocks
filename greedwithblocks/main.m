91e510450084c0741bd2e0a7740d73bfa0b59759
Mark Haskamp <mark.haskamp@pinnsg.com>
2013-10-08 22:33:37 -0400

    Initial Commit

---
+++ greedwithblocks/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  greedwithblocks
+//
+//  Created by Mark Haskamp on 10/8/13.
+//  Copyright (c) 2013 Mark Haskamp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
